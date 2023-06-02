de426c2d6d98f256f9025fd3a8b2adf3082b1536
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-06-02 22:37:32 +0400

    Updated some pages

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
+
+const experimentsQuery = queryContent('experiments').find()
+console.log(experimentsQuery)
 </script>
 
 <template>
   <main class="p-4 py-16 flex gap-2 flex-col lg:flex-row">
     <div class="drawer basis-1/5 bg-base-100 border-[1px] border-primary p-4 rounded-lg">
       <div class="drawer-side flex flex-col text-white prose prose-a:no-underline">
-        <ContentNavigation />
+        <ContentNavigation :query="experimentsQuery">
+        </ContentNavigation>
       </div>
     </div>
     <div class="flex flex-col w-full rounded-lg prose-lg px-8 grow basis-3/4 prose-li:list-disc">
-      <ContentDoc />
+      <ContentDoc v-slot="{ doc }">
+        <div class="pb-4 border-b-2 border-b-primary">
+          <h1 class="text-6xl font-boldy mb-1">
+            {{ doc.title }}
+          </h1>
+        </div>
+        <ContentRenderer :value="doc" />
+      </ContentDoc>
     </div>
   </main>
 </template>
@@ -17,10 +28,6 @@
 <style scoped lang="postcss">
 main:deep(a) {
   @apply text-primary;
-}
-
-main:deep(h1) {
-  @apply text-6xl font-bold pb-4 border-b-2 border-b-primary;
 }
 
 main:deep(pre) {
--- pages/index.vue
+++ pages/index.vue
@@ -1,102 +1,61 @@
 <script setup lang="ts">
-import { vElementHover } from '@vueuse/components';
+// import { vElementHover } from '@vueuse/components';
 
-const isArtistHovered = ref(false);
-function onArtistHovered(state: boolean) {
-    isArtistHovered.value = state;
-}
+// const isArtistHovered = ref(false);
+// function onArtistHovered(state: boolean) {
+//     isArtistHovered.value = state;
+// }
 
-const isDeveloperHovered = ref(false);
-function onDeveloperHovered(state: boolean) {
-    isDeveloperHovered.value = state;
-}
+// const isDeveloperHovered = ref(false);
+// function onDeveloperHovered(state: boolean) {
+//     isDeveloperHovered.value = state;
+// }
 
-const isGamesHovered = ref(false);
-function onGamesHovered(state: boolean) {
-    isGamesHovered.value = state;
-}
+// const isGamesHovered = ref(false);
+// function onGamesHovered(state: boolean) {
+//     isGamesHovered.value = state;
+// }
 
-const isStoriesHovered = ref(false);
-function onStoriesHovered(state: boolean) {
-    isStoriesHovered.value = state;
-}
+// const isStoriesHovered = ref(false);
+// function onStoriesHovered(state: boolean) {
+//     isStoriesHovered.value = state;
+// }
 
-const isOtherHovered = ref(false);
-function onOtherHovered(state: boolean) {
-    isOtherHovered.value = state;
-}
+// const isOtherHovered = ref(false);
+// function onOtherHovered(state: boolean) {
+//     isOtherHovered.value = state;
+// }
 
 </script>
 
 <template>
-    <main class="flex flex-col py-8 lg:py-16 items-center justify-center w-full  overflow-hidden">
+    <main class="flex flex-col py-8 lg:py-16 items-center justify-center w-full overflow-hidden h-screen">
         <section class="bg-opacity-5 w-full flex flex-col lg:flex-row items-center justify-center gap-12 relative">
             <div class="absolute w-screen h-screen hero-bg pointer-events-none"></div>
             <div class="flex flex-col justify-center items-center w-full lg:w-auto px-4">
                 <div>
-                    <h1 class="text-4xl lg:text-8xl text-outline-primary font-extrabold uppercase text-transparent">Vincent</h1>
+                    <h1 class="text-4xl lg:text-8xl text-outline-primary font-extrabold uppercase text-transparent">Vincent
+                    </h1>
                     <h2 class="text-xl lg:text-6xl font-extrabold text-primary">Amante</h2>
                 </div>
 
                 <div class="divider text-primary">I make</div>
-
-                <ul class="text-2xl w-full flex flex-col gap-0   font-thin">
-                    <li
-                    v-element-hover="onDeveloperHovered"
-                    class="py-1 px-2 cursor-pointer rounded-sm hover:bg-primary">
-                    Websites
-                    </li>
-                    <li
-                    v-element-hover="onArtistHovered"
-                    class="py-1 px-2 cursor-pointer rounded-sm hover:bg-primary">
-                        Art
-                    </li>
-                    <li
-                    v-element-hover="onGamesHovered"
-                    class="py-1 px-2 cursor-pointer rounded-sm hover:bg-primary">
-                        Games
-                    </li>
-                    <li
-                    v-element-hover="onStoriesHovered"
-                    class="py-1 px-2 cursor-pointer rounded-sm hover:bg-primary">
-                        Stories
-                    </li>
-                    <li
-                    v-element-hover="onOtherHovered"
-                    class="py-1 px-2 cursor-pointer rounded-sm hover:bg-primary">
-                        Other stuff
-                    </li>
-                </ul>
+                <h1 class="text-3xl">Arduino Code</h1>
             </div>
             <div class="relative  flex flex-col items-center px-4">
-                <div class="relative  isolate                                                                   max-w-xs lg:max-w-sm flex flex-col rounded-lg">
-                    <h1 class="absolute transform z-[-10] -translate-y-6  text-primary  left-2                  text-5xl font-bold uppercase">Hello</h1>
-                    <h1 class="absolute transform text-outline-primary text-transparent left-2 -translate-y-6   text-5xl font-bold uppercase">Hello</h1>
-                    <!-- img elements below -->
-                    <div class="w-full h-full absolute "></div>
-                    <div class="w-full h-full opacity-0 grainy-bg absolute top-0 left-0"></div>
-                    <img
-                    class="absolute top-0 w-full object-cover transition-all"
-                    :class="(isDeveloperHovered) ? 'opacity-100': 'opacity-0'"
-                    src="@/assets/images/self-web.png"/>
-                    <img
-                    class="absolute top-0 w-full object-cover transition-all"
-                    :class="(isArtistHovered) ? 'opacity-100': 'opacity-0'"
-                    src="@/assets/images/self-art.png"/>
-                    <img class="w-full object-cover" src="@/assets/images/self-pic.png"/>
-                </div>
-            </div>
-        </section>
-        <section class="w-full flex flex-col items-center bg-opacity-10 py-10 pt-16">
-            <div class=" flex flex-col items-center overflow-hidden">
-                <div class="w-4 h-4 bg-primary flex items-center justify-center rounded-full"></div>
-                <div class="w-1 h-[12rem] bg-primary flex items-center transform scale-y-120"></div>
-                <div class=" overflow-hidden h-5">
-                    <div class="w-8 h-8 bg-primary flex items-center justify-center rounded-full"></div>
-                </div>
-            </div>
-            <div class="px-4 lg:px-16 w-full">
-                <div class=" w-full border-primary border-t-2 border-solid">
+                <div class="relative isolate max-w-xs lg:max-w-sm flex flex-col rounded-lg">
+                    <div class="p-4 flex flex-col gap-2">
+                        <NuxtLink to="experiments">
+                            <h1
+                                class="text-4xl p-4 py-8 border-2 border-white border-solid rounded-lg flex items-center hover:bg-white hover:text-secondary cursor-pointer">
+                                Experiments
+                            </h1>
+                        </NuxtLink>
+                        <h1
+                            class="text-4xl p-4 py-8 border-2 border-white border-solid rounded-lg flex items-center hover:bg-white hover:text-secondary cursor-pointer">
+                            ??
+                        </h1>
+                    </div>
                 </div>
             </div>
         </section>
@@ -104,21 +63,16 @@ function onOtherHovered(state: boolean) {
 </template>
 
 <style scoped lang="postcss">
+.text-outline-primary {
+    -webkit-text-stroke: 1px;
+    -webkit-text-stroke-color: #E5724D;
+}
 
-    .hero-bg {
-background: -moz-radial-gradient(circle, rgba(182,185,255,0.06) 0%, rgba(255,255,255,0) 34%);
-background: -webkit-radial-gradient(circle, rgba(182,185,255,0.06) 0%, rgba(255,255,255,0) 34%);
-background: radial-gradient(circle, rgba(182,185,255,0.06) 0%, rgba(255,255,255,0) 34%);
-    }
-    .text-outline-primary {
-        -webkit-text-stroke: 1px;
-        -webkit-text-stroke-color: #E5724D;
-    }
-    .grainy-bg {
+.grainy-bg {
     filter: contrast(100%) brightness(100%);
     background:
-	linear-gradient(158deg, rgba(229,114,77,0.57), rgba(208,143,39,0.49)),
-	url("data:image/svg+xml,%3Csvg viewBox='0 0 268 268' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='7.66' numOctaves='1' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
+        linear-gradient(158deg, rgba(229, 114, 77, 0.57), rgba(208, 143, 39, 0.49)),
+        url("data:image/svg+xml,%3Csvg viewBox='0 0 268 268' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='7.66' numOctaves='1' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
     animation: 0.2s infinite noise;
-    }
+}
 </style>
